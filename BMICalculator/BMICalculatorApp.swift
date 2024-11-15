a5318e4d3350e9b7c1c1f6abdde1a83fa34dbaa3
Ujjwal Arora <ukarora9@gmail.com>
2024-11-16 00:02:52 +0530

    Initial Commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by Ujjwal Arora on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
